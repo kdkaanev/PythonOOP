d3872415421e5fdd818cdcefb568b5dd079be0af
kdkaanev <103933652+kdkaanev@users.noreply.github.com>
2022-07-09 09:05:23 +0300

    new commiit

--- classesAndObjects/exerciseClassesAndObjects/time.py
+++ classesAndObjects/exerciseClassesAndObjects/time.py
@@ -2,21 +2,21 @@ class Time:
     max_hours = 23
     max_minutes = 59
     max_seconds = 59
-    
+
     def __init__(self, hours, minutes, seconds):
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
-        
+
     def set_time(self, hours, minutes, seconds):
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
-    
+
     def get_time(self):
-       return f"{self.hours}:{self.minutes}:{self.seconds}"
-   
-   
+       return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
+
+
     def next_second(self):
          self.seconds += 1
          if self.seconds > self.max_seconds:
@@ -33,15 +33,7 @@ class Time:
 
 
 
-time = Time(9, 30, 59)
+time = Time(23, 59, 59)
 print(time.next_second())
 
 
-        
-        
-        
-        
-        
-        
-        
-     
